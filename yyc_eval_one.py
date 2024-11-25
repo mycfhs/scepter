7d76384bafe316c7ae037655702940f1cbc6c9d6
mycfhs <2286247133@qq.com>
2024-11-25 13:41:57 +0800

    for DreamMix

---
+++ yyc_eval_one.py
@@ -0,0 +1,124 @@
+# PYTHONPATH=. python scepter/tools/webui.py --cfg scepter/methods/studio/scepter_ui.yaml
+from torchvision.utils import save_image
+from scepter.modules.utils.config import Config
+from scepter.modules.utils.file_system import FS
+from scepter.modules.utils.logger import get_logger
+from scepter.modules.inference.largen_inference import LargenInference
+
+from scepter.studio.inference.inference_ui.largen_ui import LargenUI
+
+from PIL import Image
+import numpy as np
+from pytorch_lightning import seed_everything
+
+category = "grey_sloth_plushie"
+prompt = "a photo of a sks gray plushie raising both hands cheerfully"
+img_name = 21048
+"""
+grey_sloth_plushie
+a photo of a sks gray plushie raising both hands cheerfully
+21048
+"""
+
+tar_path  = "/media/zl/file4T/yyc/DreamMix/Fooocus_diffusers_final"
+ref_path  = "/media/zl/file4T/yyc/DreamMix/Fooocus_diffusers_final/train_data"
+
+ref_image = Image.open(f"{ref_path}/{category}/image/02.jpg").convert("RGB")
+ref_mask = Image.open(f"{ref_path}/{category}/mask/02.png").convert("L")
+
+# output_height, output_width = ref_image.size
+output_height, output_width = 1024, 1024
+
+tar_image = Image.open(f"{tar_path}/test_data/{img_name}_1.png").convert("RGB")
+tar_mask  = Image.open(f"{tar_path}/test_data/{img_name}_2.png").convert("L")
+
+# category_2 = "duck_toy"
+# tar_image = Image.open(f"{ref_path}/{category_2}/image/00.jpg").convert("RGB")
+# tar_mask = Image.open(f"{ref_path}/{category_2}/mask/00.png").convert("L")
+
+# tar_image = Image.open(f"asset/images/inpainting_text_ref/ex4_scene_im.jpg").convert("RGB")
+# tar_mask = Image.open(f"asset/images/inpainting_text_ref/ex4_scene_mask.jpg").convert("L")
+# ref_image = Image.open(f"asset/images/inpainting_text_ref/ex4_subject_im.jpg").convert("RGB")
+# ref_mask = Image.open(f"asset/images/inpainting_text_ref/ex4_subject_mask.jpg").convert("L")
+
+
+# ref_image = subject_image['background'].convert('RGB')
+# ref_mask = subject_image['layers'][0].split()[-1].convert('L')
+ref_image = np.asarray(ref_image)
+ref_mask = np.asarray(ref_mask)
+ref_mask = np.where(ref_mask > 128, 1, 0).astype(np.uint8)
+
+tar_image = np.asarray(tar_image)
+tar_mask = np.asarray(tar_mask)
+tar_mask = np.where(tar_mask > 128, 1, 0).astype(np.uint8)
+
+
+data = LargenUI.data_preprocess_inpaint(
+    None, tar_image, tar_mask, ref_image, ref_mask, True, 1.1, output_height, output_width
+)
+
+
+# init file system - modelscope
+# FS.TEMP_DIRinit_fs_client(Config(load=False, cfg_dict={'NAME': 'ModelscopeFs', 'TEMP_DIR': 'cache/data'}))
+FS.TEMP_DIRinit_fs_client(
+    Config(load=False, cfg_dict={"NAME": "ModelscopeFs", "TEMP_DIR": "cache/cache_data"})
+)   # 新版本改名字了hhh。 ui里面保存到cache data。我们就用之前下载好的，不然得重新下载。 这个在scepter_ui.yaml里面
+FS.TEMP_DIRinit_fs_client(
+    Config(load=False, cfg_dict={"NAME": "HttpFs", "TEMP_DIR": "cache/cache_data"})
+)   # load参数不知道需不需要单独设置
+
+# init model config
+logger = get_logger(name='scepter')
+cfg = Config(cfg_file='scepter/methods/studio/inference/largen/largen_pro.yaml')
+largen_infer = LargenInference(logger)
+largen_infer.init_from_cfg(cfg)
+
+input_config = {
+    "image": None,
+    "original_size_as_tuple": [output_height, output_width],
+    "target_size_as_tuple": [output_height, output_width],
+    "aesthetic_score": 6.0,
+    "negative_aesthetic_score": 2.5,
+    # "prompt": "a photo of a backpack",
+    "prompt": prompt,
+    "negative_prompt": "",
+    "prompt_prefix": "",
+    "crop_coords_top_left": [0, 0],
+    "sample": "ddim",
+    "sample_steps": 50,
+    "guide_scale": 7.5,
+    "guide_rescale": 0,
+    "discretization": "trailing",
+    "refine_sample": "ddim",
+    "refine_guide_scale": 7.5,
+    "refine_guide_rescale": 0.5,
+    "refine_discretization": "trailing",
+}
+
+# for seed in range(100):
+if True:
+    # seed = 42
+    for seed in range(100):
+        seed_everything(seed)
+        # start inference
+        output = largen_infer(
+            input=input_config,
+            num_samples=1,
+            intermediate_callback=None,
+            refine_strength=0,
+            cat_uc=True,
+            largen_state=True,
+            largen_task="Text_Subject_Guided_Inpainting",
+            largen_image_scale=1,
+            largen_tar_image=data[0],
+            largen_tar_mask=data[1],
+            largen_masked_image=data[2],
+            largen_ref_image=data[3],
+            largen_ref_mask=data[4],
+            largen_ref_clip=data[5],
+            largen_base_image=data[6],
+            largen_extra_sizes=data[7],
+            largen_bbox_yyxx=data[8],
+        )
+
+        save_image(output["images"], f"{prompt}_{seed}.png")
---
+++ yyc_eval_user_study.py
@@ -0,0 +1,172 @@
+# PYTHONPATH=. python scepter/tools/webui.py --cfg scepter/methods/studio/scepter_ui.yaml
+from torchvision.utils import save_image
+from scepter.modules.utils.config import Config
+from scepter.modules.utils.file_system import FS
+from scepter.modules.utils.logger import get_logger
+from scepter.modules.inference.largen_inference import LargenInference
+from scepter.studio.inference.inference_ui.largen_ui import LargenUI
+import os
+from PIL import Image
+import numpy as np
+import yaml
+import shutil
+from tqdm import tqdm
+from pytorch_lightning import seed_everything
+
+# 去scepter.scepter.modules.utils.distribute.__init__ 里面修改device_id  line618
+
+
+# 读取YAML文件
+with open(
+    "/home/zl/yyc_workspace/Fooocus_diffusers/categories_final.yml", "r"
+) as yml_file:
+    data = yaml.safe_load(yml_file)
+seed = 42
+category_dict = data["category_dict"]
+output_height, output_width = 1024, 1024
+category_num = 30
+
+tar_path = "/home/zl/yyc_workspace/Fooocus_diffusers/pairs_test"
+ref_path = "/home/zl/yyc_workspace/Fooocus_diffusers/train_data"
+
+all_bg = [png[:-6] for png in os.listdir(tar_path) if png.endswith("_1.png")]
+category_list = [i for i in os.listdir(ref_path) if i in category_dict.keys()]
+print(category_list)
+category_list.sort()
+print(category_list)
+# init file system - modelscope
+# FS.TEMP_DIRinit_fs_client(Config(load=False, cfg_dict={'NAME': 'ModelscopeFs', 'TEMP_DIR': 'cache/data'}))
+FS.TEMP_DIRinit_fs_client(
+    Config(
+        load=False, cfg_dict={"NAME": "ModelscopeFs", "TEMP_DIR": "cache/cache_data"}
+    )
+)  # 新版本改名字了hhh。 ui里面保存到cache data。我们就用之前下载好的，不然得重新下载。 这个在scepter_ui.yaml里面
+FS.TEMP_DIRinit_fs_client(
+    Config(load=False, cfg_dict={"NAME": "HttpFs", "TEMP_DIR": "cache/cache_data"})
+)
+
+# init model config
+logger = get_logger(name="scepter")
+cfg = Config(cfg_file="scepter/methods/studio/inference/largen/largen_pro.yaml")
+largen_infer = LargenInference(logger)
+largen_infer.init_from_cfg(cfg)
+
+"""
+bash
+cd project/scepter
+ca
+ca scepter
+python yyc_eval_4k_edit
+"""
+
+for index, category in enumerate(tqdm(category_list)):
+    save_path = f"./output/user_study/{category}"
+    if os.path.exists(save_path):
+        shutil.rmtree(save_path)
+    os.makedirs(save_path)
+    folder_path = os.path.join(ref_path, category)
+
+    input_shape = Image.open(f"{ref_path}/{category}/image/00.jpg").size
+
+    ref_image = Image.open(f"{ref_path}/{category}/image/00.jpg").convert("RGB")
+    ref_mask = Image.open(f"{ref_path}/{category}/mask/00.png").convert("L")
+
+    ref_image = np.asarray(ref_image)
+    ref_mask = np.asarray(ref_mask)
+    ref_mask = np.where(ref_mask > 128, 1, 0).astype(np.uint8)
+
+    prompt_id = 0
+    edit_prompt_num = len(category_dict[category]) - 1
+
+    # for img_name in all_bg[index::category_num]:
+    img_name = index + 1
+    for prompt in category_dict[category]:
+        seed_everything(seed)
+        prompt = prompt.replace("sks", "")
+
+        img_name = str(img_name)
+
+        # if os.path.exists(f"./{save_path}/{img_name}+{prompt}+{seed}.jpg"):
+        #     print(f"Already exists:  ./{save_path}/{img_name}+{prompt}+{seed}.jpg")
+        #     continue
+
+        tar_image = (
+            Image.open(f"{tar_path}/{img_name}_1.png")
+            .resize((1024, 1024))
+            .convert("RGB")
+        )
+        tar_mask = (
+            Image.open(f"{tar_path}/{img_name}_2.png").resize((1024, 1024)).convert("L")
+        )
+
+        # ref_image = subject_image['background'].convert('RGB')
+        # ref_mask = subject_image['layers'][0].split()[-1].convert('L')
+
+        tar_image = np.asarray(tar_image)
+        tar_mask = np.asarray(tar_mask)
+        tar_mask = np.where(tar_mask > 128, 1, 0).astype(np.uint8)
+
+        (
+            largen_tar_image,
+            largen_tar_mask,
+            largen_masked_image,
+            largen_ref_image,
+            largen_ref_mask,
+            largen_ref_clip,
+            largen_base_image,
+            largen_extra_sizes,
+            largen_bbox_yyxx,
+        ) = LargenUI.data_preprocess_inpaint(
+            None,
+            tar_image,
+            tar_mask,
+            ref_image,
+            ref_mask,
+            True,
+            1.3,
+            output_height,
+            output_width,
+        )
+
+        input_config = {
+            "image": None,
+            "original_size_as_tuple": input_shape,
+            "target_size_as_tuple": [1024, 1024],
+            "aesthetic_score": 6.0,
+            "negative_aesthetic_score": 2.5,
+            "prompt": prompt,
+            "negative_prompt": "",
+            "prompt_prefix": "",
+            "crop_coords_top_left": [0, 0],
+            "sample": "ddim",
+            "sample_steps": 50,
+            "guide_scale": 7.5,
+            "guide_rescale": 0,
+            "discretization": "trailing",
+            "refine_sample": "ddim",
+            "refine_guide_scale": 7.5,
+            "refine_guide_rescale": 0.5,
+            "refine_discretization": "trailing",
+        }
+        # start inference
+        output = largen_infer(
+            input=input_config,
+            num_samples=1,
+            intermediate_callback=None,
+            refine_strength=0,
+            cat_uc=True,
+            largen_state=True,
+            largen_task="Text_Subject_Guided_Inpainting",
+            largen_image_scale=0.5,
+            largen_tar_image=largen_tar_image,
+            largen_tar_mask=largen_tar_mask,
+            largen_masked_image=largen_masked_image,
+            largen_ref_image=largen_ref_image,
+            largen_ref_mask=largen_ref_mask,
+            largen_ref_clip=largen_ref_clip,
+            largen_base_image=largen_base_image,
+            largen_extra_sizes=largen_extra_sizes,
+            largen_bbox_yyxx=largen_bbox_yyxx,
+        )
+
+        save_image(output["images"], f"./{save_path}/{img_name}+{prompt}+{seed}.jpg")
